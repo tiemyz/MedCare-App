a458d8ee30b4f835188a107a8c6fd29d3aad815a
tiemyz <rm95145@fiap.com.br>
2023-11-24 09:20:11 -0300

    att

--- screens/InfoWatch.js
+++ screens/InfoWatch.js
@@ -1,16 +1,49 @@
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, ImageBackground, Image, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import PerfilWatch from '../assets/perfil-watch.png';
 import IconPerfil from '../assets/icon-perfil.png';
 
 const InfoWatch = () => {
+  const [infoWatch, setInfoWatch] = useState([]);
+  const [pacienteDashboard, setPacienteDashboard] = useState(null);
+
+  useEffect(() => {
+    fetchInfoWatch();
+    fetchPacienteDashboard();
+  }, []);
+
+  const fetchInfoWatch = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/api/infos/1');
+      const data = await response.json();
+      setInfoWatch(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchPacienteDashboard = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/api/usuarios/2');
+      const data = await response.json();
+      setPacienteDashboard(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  if (!infoWatch || !pacienteDashboard) {
+    return <Text>Carregando informações...</Text>;
+  }
+  
   return (
     <ImageBackground source={PerfilWatch} style={styles.background}>
 
       <View style={styles.viewSuperior}>
         <Image source={IconPerfil} style={{ width: 70, height: 70, resizeMode: 'contain'}}></Image>
         <Text style={{fontSize: 20, marginTop: 20}}>Nome do(a) paciente:</Text>
-        <Text style={{fontSize: 20}}>api</Text>
+        <Text style={{fontSize: 20}}>{pacienteDashboard.nome}</Text>
       </View>
 
       <View style={styles.viewInferior}>
@@ -18,13 +51,11 @@ const InfoWatch = () => {
 
         <View style={{margin: 30}}>
           <Text style={{fontSize: 18, textAlign: 'center'}}>Batimento cardíaco:</Text>
-          <Text style={{fontSize: 18, textAlign: 'center', color: '#A7043B', fontWeight: 'bold', margin: 10}}>api</Text>
+          <Text style={{fontSize: 18, textAlign: 'center', color: '#A7043B', fontWeight: 'bold', margin: 10}}>{infoWatch.batimento}</Text>
           <Text style={{fontSize: 18, textAlign: 'center'}}>Temperatura:</Text>
-          <Text style={{fontSize: 18, textAlign: 'center', color: '#A7043B', fontWeight: 'bold', margin: 10}}>api</Text>
+          <Text style={{fontSize: 18, textAlign: 'center', color: '#A7043B', fontWeight: 'bold', margin: 10}}>{infoWatch.temp}</Text>
           <Text style={{fontSize: 18, textAlign: 'center'}}>Umidade:</Text>
-          <Text style={{fontSize: 18, textAlign: 'center', color: '#A7043B', fontWeight: 'bold', margin: 10}}>api</Text>
-          <Text style={{fontSize: 18, textAlign: 'center'}}>Localização: </Text>
-          <Text style={{fontSize: 18, textAlign: 'center', color: '#A7043B', fontWeight: 'bold', margin: 10}}>api</Text>
+          <Text style={{fontSize: 18, textAlign: 'center', color: '#A7043B', fontWeight: 'bold', margin: 10}}>{infoWatch.humidade}</Text>
         </View>
       </View>     
     </ImageBackground>
